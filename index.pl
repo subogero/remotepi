92640878b0af951aab4d3707a400eeb4bc2c186d
SZABO Gergely <szg@subogero.com>
2013-09-22 21:49:16 +0200

    Separate body and style from index.pl

--- index.pl
+++ index.pl
@@ -20,38 +20,15 @@ if ($query eq 'S') {
     exit 0;
 }
 
-print <<HTML;
+print <<HEAD2;
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style>
-body { font-family:Monospace; margin:0 }
-div.st {
-  background-color:#ffc040;
-  font-size:80%; font-weight:bold; text-align:center; line-height:200%
-}
-div.con { text-align:center }
-a:link { text-decoration:none }
-a:visited { text-decoration:none }
-</style>
 <script src="status.js"></script>
 <script src="controls.js"></script>
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
-<div class="st" id="st">Status</div>
-<script>getStatus();</script>
-<div class="con"><p>
-<button onclick="controls('N')">N</button>
-<button onclick="controls('R')">R</button>
-<button onclick="controls('r')">r</button>
-<button onclick="controls('.')">.</button>
-<button onclick="controls('p')">p</button>
-<button onclick="controls('f')">f</button>
-<button onclick="controls('F')">F</button>
-<button onclick="controls('n')">n</button>
-</p>
-<p>
-<a href="?/home">[Raspberry]</a>
-<a href="?fm">[rpi.fm]</a>
-</p></div>
-</body>
-</html>
-HTML
+HEAD2
+if (open BODY, "body.html") {
+    print while <BODY>;
+    close BODY;
+}
+print "</html>\n";
