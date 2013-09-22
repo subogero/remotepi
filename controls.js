7a3a63ef5bad238672d79848a261bc752ae49a1c
SZABO Gergely <szg@subogero.com>
2013-09-22 23:24:55 +0200

    Add home and rpi.fm divs and toggle buttons

--- controls.js
+++ controls.js
@@ -4,3 +4,16 @@ function controls(cmd) {
   req.open("GET", "?" + cmd, true);
   req.send();
 }
+
+function browse(what) {
+  var home = document.getElementById("home");
+  var fm   = document.getElementById("fm");
+  if (what == "home") {
+    home.style.display = 'block';
+    fm.style.display = 'none';
+  }
+  else if (what == "fm") {
+    home.style.display = 'none';
+    fm.style.display = 'block';
+  }
+}
