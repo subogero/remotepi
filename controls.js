3fbf2c5352f8acd8e303a0b6baa21a70de7d51a9
SZABO Gergely <szg@subogero.com>
2013-09-23 09:11:38 +0200

    Basic directory listing
    When clicking the Raspberry button,
    list the dir specified in /etc/remotepi.cfg or /home.

--- controls.js
+++ controls.js
@@ -5,12 +5,24 @@ function controls(cmd) {
   req.send();
 }
 
+function run_home() {
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function() {
+    if (req.readyState == 4 && req.status == 200) {
+      document.getElementById("home").innerHTML = req.responseText;
+    }
+  }
+  req.open("GET", "?home /", true);
+  req.send();
+}
+
 function browse(what) {
   var home = document.getElementById("home");
   var fm   = document.getElementById("fm");
   if (what == "home") {
     home.style.display = 'block';
     fm.style.display = 'none';
+    run_home();
   }
   else if (what == "fm") {
     home.style.display = 'none';
--- index.pl
+++ index.pl
@@ -1,5 +1,10 @@
 #!/usr/bin/perl
 # C) 2013 SZABO Gergely <szg@subogero.com> GNU AGPL v3
+use HTML::Entities;
+use CGI::Carp qw(fatalsToBrowser);
+sub browse_home;
+
+# Common head part for normal page and AJAX responses
 print <<HEAD;
 Content-type: text/html
 
@@ -9,18 +14,29 @@ Content-type: text/html
 <meta charset="UTF-8">
 HEAD
 
-my $query = $ENV{QUERY_STRING};
-if ($query eq 'S') {
+# Handle AJAX requests
+$get_req = $ENV{QUERY_STRING};
+if ($get_req eq 'S') {
     print "</head><body>";
-    system "omxd", $query;
+    system "omxd", $get_req;
     print "</body></html>";
     exit 0;
-} elsif ($query =~ /^[NRr.pfFn]$/) {
+} elsif ($get_req =~ /^[NRr.pfFn]$/) {
     print "</head><body></body></html>";
-    `omxd $query` if $query;
+    `omxd $get_req` if $get_req;
+    exit 0;
+} elsif ($get_req =~ /^home/) {
+    (my $dir = decode_entities $get_req) =~ s/^home //;
+    print "</head>";
+    browse_home $dir;
+    print "</html>";
+    exit 0;
+} elsif ($get_req) {
+    print "<!-- $get_req -->\n";
     exit 0;
 }
 
+# Or continue the normal page
 print <<HEAD2;
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="status.js"></script>
@@ -33,3 +49,29 @@ if (open BODY, "body.html") {
     close BODY;
 }
 print "</html>\n";
+
+# Browse Raspberry Pi
+sub browse_home {
+    my $dir = shift;
+    my $root;
+    if (open CFG, "/etc/remotepi.cfg") {
+        $root = <CFG>;
+        chomp $root;
+        close CFG;
+    } else {
+        $root = "/home";
+    }
+    $dir = $dir =~ /^\./ ? $root : "$root/$dir";
+    $dir =~ s|(.+)/.+|$1| while ! -d $dir;
+    print "<body>\n";
+    opendir DIR, $dir;
+    push @files, $_ while readdir DIR;
+    closedir DIR;
+    foreach (sort @files) {
+        next if /^\.$/;
+        next if /^\.\w/;
+        next if /^\.\.$/ && $dir eq $root;
+        print "<p>$_</p>\n";
+    }
+    print "</body>\n";
+}
