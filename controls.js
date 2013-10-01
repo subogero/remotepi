f41d6771abaf8273420058a72b000fd01666ef50
SZABO Gergely <szg@subogero.com>
2013-10-01 09:12:44 +0200

    Style with tabs for buttons/home/fm

--- controls.js
+++ controls.js
@@ -6,22 +6,35 @@ function controls(cmd) {
 }
 
 function browse(what) {
+  var list = document.getElementById("list");
   var home = document.getElementById("home");
   var fm   = document.getElementById("fm");
+  var blist = document.getElementById("blist");
+  var bhome = document.getElementById("bhome");
+  var bfm   = document.getElementById("bfm");
   if (what == "list") {
     list.style.display = 'block';
+    blist.className = 'tabhi';
     home.style.display = 'none';
+    bhome.className = 'tablo';
     fm.style.display = 'none';
+    bfm.className = 'tablo';
   }
   else if (what == "home") {
     rpi.ls();
     list.style.display = 'none';
+    blist.className = 'tablo';
     home.style.display = 'block';
+    bhome.className = 'tabhi';
     fm.style.display = 'none';
+    bfm.className = 'tablo';
   }
   else if (what == "fm") {
     list.style.display = 'none';
+    blist.className = 'tablo';
     home.style.display = 'none';
+    bhome.className = 'tablo';
     fm.style.display = 'block';
+    bfm.className = 'tabhi';
   }
 }
