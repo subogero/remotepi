df86911755d7f9a3a0c1ced40cc2dd383aba28e0
SZABO Gergely <szg@subogero.com>
2014-12-08 21:41:40 +0100

    raspberry.js indentation #2

--- raspberry.js
+++ raspberry.js
@@ -2,29 +2,29 @@ rpi = {};
 rpi.pwd = "/";
 
 rpi.ls = function() {
-  var req = new XMLHttpRequest();
-  req.onreadystatechange = function() {
-    if (req.readyState == 4 && req.status == 200) {
-      document.getElementById("home").innerHTML = req.responseText;
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if (req.readyState == 4 && req.status == 200) {
+            document.getElementById("home").innerHTML = req.responseText;
+        }
     }
-  }
-  req.open("GET", "api.pl?home " + this.pwd, true);
-  req.send();
+    req.open("GET", "api.pl?home " + this.pwd, true);
+    req.send();
 }
 
 rpi.cd = function(dir) {
-  if (dir === "..") {
-    var regex = /[^\/]+\/$/;
-    this.pwd = this.pwd.replace(regex, "");
-  } else {
-    this.pwd += dir + "/";
-  }
-  this.ls();
+    if (dir === "..") {
+        var regex = /[^\/]+\/$/;
+        this.pwd = this.pwd.replace(regex, "");
+    } else {
+        this.pwd += dir + "/";
+    }
+    this.ls();
 }
 
 rpi.op = function(cmd, file) {
-  var req = new XMLHttpRequest();
-  var uri = "api.pl?" + cmd + " " + this.pwd + file;
-  req.open("GET", uri, true);
-  req.send();
+    var req = new XMLHttpRequest();
+    var uri = "api.pl?" + cmd + " " + this.pwd + file;
+    req.open("GET", uri, true);
+    req.send();
 }
