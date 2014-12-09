84cd46a146572eea78a6f320dad01b011c09a11b
SZABO Gergely <szg@subogero.com>
2014-12-09 08:32:09 +0100

    fm.js indentation #2

--- fm.js
+++ fm.js
@@ -2,33 +2,33 @@ rpifm = {};
 rpifm.cmds = "";
 
 rpifm.sendcmds = function() {
-  document.getElementById("fm").innerHTML = "Waiting for rpi.fm...";
-  var req = new XMLHttpRequest();
-  req.onreadystatechange = function() {
-    if (req.readyState == 4 && req.status == 200) {
-      document.getElementById("fm").innerHTML = req.responseText;
+    document.getElementById("fm").innerHTML = "Waiting for rpi.fm...";
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if (req.readyState == 4 && req.status == 200) {
+            document.getElementById("fm").innerHTML = req.responseText;
+        }
     }
-  }
-  req.open("GET", "api.pl?fm " + this.cmds, true);
-  req.send();
+    req.open("GET", "api.pl?fm " + this.cmds, true);
+    req.send();
 }
 
 rpifm.cmd = function(command) {
-  this.cmds = (command + "%0a");
-  this.sendcmds();
+    this.cmds = (command + "%0a");
+    this.sendcmds();
 }
 
 rpifm.addcmd = function() {
-  for (var i = 0; i < arguments.length; i++) {
-    this.cmds += (arguments[i] + "%0a");
-  }
-  this.sendcmds();
+    for (var i = 0; i < arguments.length; i++) {
+        this.cmds += (arguments[i] + "%0a");
+    }
+    this.sendcmds();
 }
 
 rpifm.lastcmd = function() {
-  for (var i = 0; i < arguments.length; i++) {
-    this.cmds += (arguments[i] + "%0a");
-  }
-  this.sendcmds();
-  this.cmds = "";
+    for (var i = 0; i < arguments.length; i++) {
+        this.cmds += (arguments[i] + "%0a");
+    }
+    this.sendcmds();
+    this.cmds = "";
 }
