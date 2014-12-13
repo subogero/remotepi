eace73eeaf0e4ee628733258a5eee0b8a5bf732f
SZABO Gergely <szg@subogero.com>
2014-12-13 19:47:42 +0100

    u2b.js indentation #2

--- u2b.js
+++ u2b.js
@@ -1,20 +1,20 @@
 u2b = {};
 
 u2b.search = function(query) {
-  document.getElementById("ythits").innerHTML = "Waiting for YouTube...";
-  var req = new XMLHttpRequest();
-  req.onreadystatechange = function() {
-    if (req.readyState == 4 && req.status == 200) {
-      document.getElementById("ythits").innerHTML = req.responseText;
+    document.getElementById("ythits").innerHTML = "Waiting for YouTube...";
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if (req.readyState == 4 && req.status == 200) {
+            document.getElementById("ythits").innerHTML = req.responseText;
+        }
     }
-  }
-  req.open("GET", "api.pl?yt search " + query, true);
-  req.send();
+    req.open("GET", "api.pl?yt search " + query, true);
+    req.send();
 }
 
 u2b.op = function(cmd, file) {
-  var req = new XMLHttpRequest();
-  var uri = "api.pl?yt " + cmd + " " + file;
-  req.open("GET", uri, true);
-  req.send();
+    var req = new XMLHttpRequest();
+    var uri = "api.pl?yt " + cmd + " " + file;
+    req.open("GET", uri, true);
+    req.send();
 }
